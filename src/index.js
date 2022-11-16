7ce8b94eeafffb7e87614380f4baa5f737d18f8d
searchme86 <searchme86@gmail.com>
2022-11-16 17:53:11 +0900

    👏🏻 test a mini-css-extract-plugin for webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
+import Heading from './components/heading/heading.js';
 
 const helloWorldButton = new HelloWorldButton();
+const heading = new Heading();
+
 helloWorldButton.render();
+heading.render();
