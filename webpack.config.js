673e0442e2f4178b92966d38587153371d0c51d2
searchme86 <searchme86@gmail.com>
2022-11-16 15:43:13 +0900

    🔧  add rule for asset,image

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,16 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
   },
   mode: 'none',
+  module: {
+    rules: [
+      // {
+      //   test: /\.(ttf)$/,
+      //   type: 'asset/resource',
+      // },
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
