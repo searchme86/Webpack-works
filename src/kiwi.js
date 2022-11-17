a753b8a61b38b59315b7a8a907a40c225a24426a
searchme86 <searchme86@gmail.com>
2022-11-17 14:21:41 +0900

    👏🏻 add files to test HtmlWebpackPlugin

---
+++ src/kiwi.js
@@ -0,0 +1,8 @@
+import Heading from './components/heading/heading.js';
+import KiwiImgae from './components/kiwi-image/kiwi-image.js';
+
+const heading = new Heading();
+heading.render();
+
+const kiwiImage = new KiwiImgae();
+kiwiImage.render();
