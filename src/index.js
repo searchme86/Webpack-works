df3127162998765093a7079169e6c35e666eb7cf
searchme86 <searchme86@gmail.com>
2022-11-17 14:20:25 +0900

    🗑️ delete files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
-import Heading from './components/heading/heading.js';
-
-const helloWorldButton = new HelloWorldButton();
-const heading = new Heading();
-
-helloWorldButton.render();
-heading.render();
