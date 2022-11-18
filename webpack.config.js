5ac6c3dacc98a2817fb483fe66157e21201d0eed
searchme86 <searchme86@gmail.com>
2022-11-18 13:55:59 +0900

    ✨ config a webpack.cofig.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: '/.ts$/',
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: 'public',
+  },
+};
