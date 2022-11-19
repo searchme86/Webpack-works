6708e02cedee7b3b2051312775bacbd291803a06
searchme86 <searchme86@gmail.com>
2022-11-19 13:41:15 +0900

    🔧 make a seperate webpack config file for dev/pro

---
+++ webpack.dev.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
+  },
+  devtool: 'inline-source-map',
+};
