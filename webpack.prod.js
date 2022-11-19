5c7c1a59b41b5aa0586339742ffd0e11759bf45f
searchme86 <searchme86@gmail.com>
2022-11-19 13:41:15 +0900

    🔧 make a seperate webpack config file for dev/pro

---
+++ webpack.prod.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: '[contenthash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
+  },
+  devtool: 'inline-source-map',
+  plugin: [new CleanWebpackPlugin()],
+};
